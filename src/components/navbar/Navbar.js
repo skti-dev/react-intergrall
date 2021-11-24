e63bbb218c1767d4a18b6b5489c18c9a7d057851
Augusto Seabra <augusto.seabra@uranet.com.br>
2021-11-24 14:56:44 -0300

    Adding methods

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import Search from "./Search";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center shadow-sm bg-gradient-to-t from-gray-100">
+    <nav className="flex justify-between items-center shadow-sm bg-gradient-to-b from-gray-100">
       <Search />
       <IconSettings />
     </nav>
--- src/components/settings/BackgroundChanger.js
+++ src/components/settings/BackgroundChanger.js
@@ -17,11 +17,18 @@ const BackgroundChanger = () => {
   const clickHandler = () => {
     try {
       setShowModal(true);
-      // document.querySelector("#file").click()
     }catch(e) {
       console.error("Error activating background edition")
       console.error(e)
     }
+  }
+
+  const openFileSelection = () => {
+    document.querySelector("#file").click();
+  }
+
+  const openColorSelection = () => {
+    document.querySelector("#color").click();
   }
 
   const fileChangeHandler = (fileList) => {
@@ -30,15 +37,19 @@ const BackgroundChanger = () => {
       const fileReader = new FileReader()
       fileReader.readAsDataURL(file)
       fileReader.onload = () => setDesktopBackground(fileReader.result)
+      setShowModal(false)
     }catch(e) {
       console.error("Error on file change handler")
       console.error(e)
     }
   }
 
-  const colorChangeHandler = (e) => {
+  const colorChangeHandler = (color) => {
     try {
-      console.log({ e })
+      const desktop = document.querySelector("#desktop")
+      desktop.style.cssText = `
+        background-color: ${color};
+      `
     }catch(e) {
       console.error("Error on color change handler")
       console.error(e)
@@ -64,7 +75,7 @@ const BackgroundChanger = () => {
         type="color"
         name="input-color"
         id="color"
-        onChange={(e) => colorChangeHandler(e)}
+        onChange={(e) => colorChangeHandler(e.target.value)}
       />
       {showModal && (
         <>
@@ -75,7 +86,7 @@ const BackgroundChanger = () => {
                   <h3 className="text-3xl font-semibold"> Personalização </h3>
                 </div>
                 <div className="relative flex-auto w-100">
-                  <div className="w-100 flex items-center justify-between p-3 cursor-pointer default-hover-2">
+                  <div className="w-100 flex items-center justify-between p-3 cursor-pointer default-hover-2" onClick={openColorSelection}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6">
                       <path fill="none" d="M0 0h24v24H0z"/>
                       <path d="M12 2c5.522 0 10 3.978 10 8.889a5.558 5.558 0 0 1-5.556 5.555h-1.966c-.922 0-1.667.745-1.667 1.667 0 .422.167.811.422 1.1.267.3.434.689.434 1.122C13.667 21.256 12.9 22 12 22 6.478 22 2 17.522 2 12S6.478 2 12 2zm-1.189 16.111a3.664 3.664 0 0 1 3.667-3.667h1.966A3.558 3.558 0 0 0 20 10.89C20 7.139 16.468 4 12 4a8 8 0 0 0-.676 15.972 3.648 3.648 0 0 1-.513-1.86zM7.5 12a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm9 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM12 9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
@@ -89,10 +100,10 @@ const BackgroundChanger = () => {
                       <path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z"/>
                     </svg>
                   </div>
-                  <div className="flex items-center p-3 mb-3 justify-between cursor-pointer default-hover-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                  </svg>
+                  <div className="flex items-center p-3 mb-3 justify-between cursor-pointer default-hover-2" onClick={openFileSelection}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
                     <div className="ml-3">
                       <h3 className="font-semibold"> Imagens  </h3>
                       <p className="text-gray-500"> Selecione uma imagem como plano de fundo </p>

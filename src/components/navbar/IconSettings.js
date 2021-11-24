46cf02981b1c530b3c3539ebc17eeb5c50322308
Augusto Seabra <augusto.seabra@uranet.com.br>
2021-11-24 13:51:31 -0300

    Adding icons

--- src/components/navbar/IconSettings.js
+++ src/components/navbar/IconSettings.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import Settings from "../settings/Settings";
 
 const IconSettings = () => {
@@ -11,7 +9,9 @@ const IconSettings = () => {
 
   return (
     <div className="flex p-2 relative">
-      <FontAwesomeIcon icon={faUserCircle} className="text-3xl text-i-black-800 cursor-pointer default-hover" onClick={() => toggleSettings()} />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-8 h-8 cursor-pointer default-hover"  onClick={() => toggleSettings()}>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
       <Settings state={isSettingsOpen} name="Augusto Seabra" login="RE028771" />
     </div>
   );
--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Hamburguer from "../menu/Hamburguer";
 
 const Search = () => {
@@ -7,7 +5,9 @@ const Search = () => {
     <div className="flex items-center bg-white m-2 p-2 rounded shadow-sm">
       <Hamburguer />
       <input className="focus:outline-none" name="search" placeholder="Pesquisar" />
-      <FontAwesomeIcon icon={faSearch} className="ml-2 text-xl text-i-black-800 cursor-pointer transition duration-300 opacity-80 hover:opacity-100" />
+      <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2 cursor-pointer transition duration-300 opacity-80 hover:opacity-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
     </div>
   );
 };
